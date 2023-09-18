f456860d8713c5019d8a0969866a7d419319e6cd
Hagitm3971 <hagitm3971@gmail.com>
2023-09-18 16:12:06 +0300

    screen

--- screen.py
+++ screen.py
@@ -1,28 +1,37 @@
 import pygame
 import consts
-
+import random
 
 def create_screen():
+    #screen
     pygame.init()
     size = (consts.BOARD_GRID_ROWS, consts.BOARD_GRID_COLS)
     screen = pygame.display.set_mode(size)
     screen.fill(consts.FIELD_COLOR)
-    pygame.display.flip()
+    pygame.display.update()
 
+    #grass
+    grass = pygame.image.load('grass.png')
+    resized_grass_image = pygame.transform.scale(grass, (45, 30))
+    for i in range(20):
+        x = random.randint(0, 900)
+        y = random.randint(0, 450)
+        screen.blit(resized_grass_image, (x, y))
 
-create_screen()
-status = True
-while (status):
+    #flag
+    flag = pygame.image.load('flag.png')
+    resized_flag_image = pygame.transform.scale(flag, (60, 50))
+    screen.blit(resized_flag_image, (940,447))
 
-    # iterate over the list of Event objects
-    # that was returned by pygame.event.get() method.
-    for i in pygame.event.get():
+    soldier = pygame.image.load('soldier.png')
+    resized_soldier_image = pygame.transform.scale(soldier, (75, 68))
+    screen.blit(resized_soldier_image, (0, 5))
 
-        # if event object type is QUIT
-        # then quitting the pygame
-        # and program both.
-        if i.type == pygame.QUIT:
-            status = False
+    pygame.display.update()
 
-# deactivates the pygame library
-pygame.quit()
+    status = True
+    while (status):
+        for i in pygame.event.get():
+            if i.type == pygame.QUIT:
+                status = False
+    pygame.quit()
