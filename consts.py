e0158ff114711b3867d39d495ca29d6323cc56db
amitkoren <amit.koren.p@gmail.com>
2023-09-18 11:37:32 +0300

    current consts

--- consts.py
+++ consts.py
@@ -4,8 +4,10 @@ MESSAGE = "Welcome to The Flag game. \n Have Fun!"
 WIN_MSG = ""
 LOSE_MSG = ""
 
-BOARD_WIDTH = 25
-BOARD_LENGTH = 50
+GRID_SIZE = 20
+BOARD_GRID_ROWS = 50 * GRID_SIZE
+BOARD_GRID_COLS = 25 * GRID_SIZE
+
 PLAYER_WIDTH = 2
 PLAYER_LENGTH = 4
 FLAG_WIDTH = 4
@@ -14,11 +16,13 @@ MINE_WIDTH = 3
 MINE_LENGTH = 1
 
 PLAYER_LOCATION = [0, 0]
-FLAG_LOCATION = ((BOARD_WIDTH - FLAG_WIDTH), (BOARD_LENGTH - FLAG_LENGTH))
+FLAG_LOCATION = ((BOARD_GRID_ROWS - FLAG_WIDTH), (BOARD_GRID_COLS - FLAG_LENGTH))
 
 PLAYER_IMG = pygame.image.load("bin/soldier.png")
 MINE_IMG = pygame.image.load("bin/mine.png")
 GRASS_IMG = pygame.image.load("bin/grass.png")
 FLAG_IMG = pygame.image.load("bin/flag.png")
+
+PLAYER_IMG = pygame.transform.scale(PLAYER_IMG, (PLAYER_WIDTH, PLAYER_LENGTH))
 
 FIELD_COLOR = (46, 139, 87)
--- game_field.py
+++ game_field.py
@@ -3,4 +3,11 @@
 # ומוקשים.
 # בנוסף, הוא יכיל את מתודה ליצירת המוקשים במיקום אקראי, ויצירת הדגל, ומתודות נוספות
 # הקשורות במוקשים ודגל.
+import pygame
+import consts
+
+
+screen = pygame.display.set_mode((consts.BOARD_GRID_ROWS, consts.BOARD_GRID_COLS))
+pygame.display.set_caption("Pixel to Square")
+pygame.display.flip()
 
