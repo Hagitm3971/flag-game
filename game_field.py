0e44cea8a3f05277ecf45c55ccc122f0d31c1b2c
amitkoren <amit.koren.p@gmail.com>
2023-09-18 11:50:12 +0300

    current state

--- game_field.py
+++ game_field.py
@@ -3,11 +3,4 @@
 # ומוקשים.
 # בנוסף, הוא יכיל את מתודה ליצירת המוקשים במיקום אקראי, ויצירת הדגל, ומתודות נוספות
 # הקשורות במוקשים ודגל.
-import pygame
-import consts
-
-
-screen = pygame.display.set_mode((consts.BOARD_GRID_ROWS, consts.BOARD_GRID_COLS))
-pygame.display.set_caption("Pixel to Square")
-pygame.display.flip()
 
--- screen.py
+++ screen.py
@@ -1,12 +1,28 @@
 import pygame
 import consts
 
+
 def create_screen():
     pygame.init()
     size = (consts.BOARD_GRID_ROWS, consts.BOARD_GRID_COLS)
     screen = pygame.display.set_mode(size)
-    while True:
-        screen.fill(consts.FIELD_COLOR)
-        pygame.display.flip()
+    screen.fill(consts.FIELD_COLOR)
+    pygame.display.flip()
 
 
+create_screen()
+status = True
+while (status):
+
+    # iterate over the list of Event objects
+    # that was returned by pygame.event.get() method.
+    for i in pygame.event.get():
+
+        # if event object type is QUIT
+        # then quitting the pygame
+        # and program both.
+        if i.type == pygame.QUIT:
+            status = False
+
+# deactivates the pygame library
+pygame.quit()
