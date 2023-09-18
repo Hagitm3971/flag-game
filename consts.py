a78dc771cd26a26e510551d0ee87e5c8fe6965c4
amitkoren <amit.koren.p@gmail.com>
2023-09-18 10:55:34 +0300

    current consts

--- consts.py
+++ consts.py
@@ -1,12 +1,24 @@
+import pygame
+
 MESSAGE = "Welcome to The Flag game. \n Have Fun!"
+WIN_MSG = ""
+LOSE_MSG = ""
+
 BOARD_WIDTH = 25
 BOARD_LENGTH = 50
 PLAYER_WIDTH = 2
 PLAYER_LENGTH = 4
 FLAG_WIDTH = 4
 FLAG_LENGTH = 3
-FLAG_LOCATION = ((BOARD_WIDTH - FLAG_WIDTH), (BOARD_LENGTH - FLAG_LENGTH))
 MINE_WIDTH = 3
 MINE_LENGTH = 1
-WIN_MSG = ""
-LOSE_MSG = ""
+
+PLAYER_LOCATION = [0, 0]
+FLAG_LOCATION = ((BOARD_WIDTH - FLAG_WIDTH), (BOARD_LENGTH - FLAG_LENGTH))
+
+PLAYER_IMG = pygame.image.load("bin/soldier.png")
+MINE_IMG = pygame.image.load("bin/mine.png")
+GRASS_IMG = pygame.image.load("bin/grass.png")
+FLAG_IMG = pygame.image.load("bin/flag.png")
+
+FIELD_COLOR = (46, 139, 87)
